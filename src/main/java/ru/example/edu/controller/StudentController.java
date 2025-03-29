20e9c072586b8c1d89207851f44fa7b81a5d2288
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2025-03-29 15:52:35 +0300

    add repository

--- src/main/java/ru/example/edu/controller/StudentController.java
+++ src/main/java/ru/example/edu/controller/StudentController.java
@@ -1,36 +1,38 @@
 package ru.example.edu.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.example.edu.model.Student;
+import ru.example.edu.repository.StudentRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class StudentController {
-    private List<Student> studentList = new ArrayList<>();
+    private final StudentRepository repository;
     private long countId = 0;
 
     @GetMapping("/student")
     public List<Student> getStudentList() {
-        return studentList;
+        return repository.getStudentList();
     }
 
     @PostMapping("/student")
     public long saveStudent(@RequestParam String name, @RequestParam String email, @RequestParam int age) {
         countId = countId + 1;
         Student savedStudent = new Student(countId, name, email, age);
-        studentList.add(savedStudent);
-        return savedStudent.getId();
+        return repository.save(savedStudent);
     }
 
     @GetMapping("/student/{id}")
     public Student getStudentById(@PathVariable long id) {
-
-        for (Student student : studentList) {
-            if (student.getId() == id) return student;
-        }
-        return null;
+        return repository.getById(id);
     }
 
+    @DeleteMapping("/student/{id}")
+    public void deleteStudentById(@PathVariable long id) {
+        repository.deleteById(id);
+    }
 }
