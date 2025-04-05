6a7249970b3534c8c11fb93dff0b66ea9ae0294b
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2025-04-05 13:31:02 +0300

    add student table

--- src/main/java/ru/example/edu/model/Student.java
+++ src/main/java/ru/example/edu/model/Student.java
@@ -1,10 +1,27 @@
 package ru.example.edu.model;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "student")
+@NoArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "email")
     private String email;
+    @Column(name = "age")
     private int age;
+
+    public Student(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
 
     public Student(long id, String name, String email, int age) {
         this.id = id;
